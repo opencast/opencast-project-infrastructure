3d00a774929bf3dc4bc2a2e633aa43be688bbebe
Greg Logan <gregorydlogan@gmail.com>
2018-03-19 20:21:07 -0600

    script work

--- ansible-buildbot-cluster/roles/buildbot-worker/templates/centsetup.sh
+++ ansible-buildbot-cluster/roles/buildbot-worker/templates/centsetup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -uxe
+
 echo "[opencast]
 name = Opencast el 7 Repository
 baseurl  = {{ oc_rpm_repo_url }}
@@ -18,3 +20,24 @@ enabled  = 1
 gpgcheck = 1
 gpgkey = {{ oc_rpm_repo_key }}
 " | tee /etc/yum.repos.d/opencast.repo
+
+echo "[opencast-testing]
+name = Opencast el 7 Testing Repository
+baseurl  = {{ oc_rpm_repo_url_testing }}
+username = {{ repo_username }}
+password = {{ repo_password }}
+enabled  = 1
+gpgcheck = 1
+gpgkey = {{ oc_rpm_repo_key }}
+
+[opencast-testing-noarch]
+name = Opencast el 7 Testing Repository - noarch
+baseurl  = {{ oc_rpm_repo_url_noarch_testing }}
+username = {{ repo_username }}
+password = {{ repo_password }}
+enabled  = 1
+gpgcheck = 1
+gpgkey = {{ oc_rpm_repo_key }}
+" | tee /etc/yum.repos.d/opencast-testing.repo
+yum update
+yum install -y ffmpeg
--- ansible-buildbot-cluster/roles/buildbot-worker/templates/debsetup.sh
+++ ansible-buildbot-cluster/roles/buildbot-worker/templates/debsetup.sh
@@ -2,7 +2,8 @@
 
 set -uxe
 
-apt-get update
-apt-get install -y wget ca-certificates apt-transport-https
 wget -qO - {{ oc_deb_repo_key }} | apt-key add -
 echo "{{ oc_deb_repo_url }}" | tee /etc/apt/sources.list.d/opencast.list
+echo "{{ oc_deb_repo_url_testing }}" | tee /etc/apt/sources.list.d/opencast-testing.list
+apt-get update
+apt-get install -y ffmpeg
