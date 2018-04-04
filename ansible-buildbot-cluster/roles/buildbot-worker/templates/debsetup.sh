b5562ec6b831d507642a2f0e04fadba57d672908
Greg Logan <gregorydlogan@gmail.com>
2018-04-04 11:22:24 -0600

    Removing import of ffmpeg from worker, fixing up some ansible and removing cruft.

--- ansible-buildbot-cluster/roles/buildbot-worker/templates/debsetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -uxe
-
-wget -qO - {{ oc_deb_repo_key }} | apt-key add -
-echo "{{ oc_deb_repo_url }}" | tee /etc/apt/sources.list.d/opencast.list
-echo "{{ oc_deb_repo_url_testing }}" | tee /etc/apt/sources.list.d/opencast-testing.list
-apt-get update
-apt-get install -y ffmpeg
