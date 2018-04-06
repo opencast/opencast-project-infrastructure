4c2adb50a55d1c4b13760b8ff5e3561eca253a73
Greg Logan <gregorydlogan@gmail.com>
2018-04-06 09:56:13 -0600

    Making life easier building the docker containers

---
+++ docker-qa-images/build-container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 IMAGE_OWNER BASE_OS IMAGE_TYPE"
+  echo " eg: $0 greglogan deb9 worker"
+  exit 1
+fi
+
+set -uxe
+
+IMAGE_TAG="$1/ocqa-$2-$3"
+cd ocqa-$2-$3
+docker build -t $IMAGE_TAG .
+docker push $IMAGE_TAG
+cd ..
+
--- docker-qa-images/build-workers.sh
+++ docker-qa-images/build-workers.sh
@@ -5,9 +5,7 @@ set -uxe
 DOCKER_OWNER=greglogan
 
 doBuild() {
-  cd ocqa-$1-$2
-  docker build -t $DOCKER_OWNER/$(basename `pwd`) .
-  cd ..
+  bash build-container.sh $DOCKER_OWNER $1 $2
 }
 
 doBuildWorker() {
