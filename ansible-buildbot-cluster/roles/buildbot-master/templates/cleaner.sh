88127b58689d23cd6182c0e1fe93cece6aa79579
Greg Logan <gregorydlogan@gmail.com>
2018-05-07 09:41:03 -0600

    Fixing cleanup script to actually clean things rather than throw errors...

--- ansible-buildbot-cluster/roles/buildbot-master/templates/cleaner.sh
+++ ansible-buildbot-cluster/roles/buildbot-master/templates/cleaner.sh
@@ -13,4 +13,4 @@
 #  build r
 #Find all directories under the top level branch dirs, modified outside of the last 30 days
 #Then delete them
-find "{{ disk_base }}" -mindepth 2 -maxdepth 2 -type d,l -ctime {{ keep_artifacts }} -exec rm -rf "{}" \;
+find "{{ disk_base }}" -mindepth 2 -maxdepth 2 -type d -ctime {{ keep_artifacts }} -exec rm -rf "{}" \;
