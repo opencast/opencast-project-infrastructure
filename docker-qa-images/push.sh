bc818819b7b0cdcd4c43e421852c94a89c19eebe
Greg Logan <gregorydlogan@gmail.com>
2021-04-27 11:22:37 -0600

    Adding an easier override for tags and pushes

--- docker-qa-images/push.sh
+++ docker-qa-images/push.sh
@@ -4,11 +4,14 @@ set -uxe
 
 DOCKER_OWNER=greglogan
 TAG=latest
-if [ $# -gt 1 ]; then
-  echo "Usage: $0 [TAG]"
+if [ $# -gt 2 -o $# -lt 1 ]; then
+  echo "Usage: $0 TAG [upstream]"
   exit 1
 elif [ $# -eq 1 ]; then
   TAG=$1
+elif [ $# -eq 2 ]; then
+  TAG=$1
+  DOCKER_OWNER=$2
 fi
 
 doPush() {
--- docker-qa-images/tag.sh
+++ docker-qa-images/tag.sh
@@ -2,13 +2,17 @@
 
 set -uxe
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 TAG"
-    exit 1
-fi
-
 DOCKER_OWNER=greglogan
-TAG=$1
+TAG=latest
+if [ $# -gt 2 -o $# -lt 1 ]; then
+  echo "Usage: $0 TAG [upstream]"
+  exit 1
+elif [ $# -eq 1 ]; then
+  TAG=$1
+elif [ $# -eq 2 ]; then
+  TAG=$1
+  DOCKER_OWNER=$2
+fi
 
 doTag() {
   bash tag-container.sh $DOCKER_OWNER $1 $2 $TAG
