861b8d26c85a9a6caa60f6623dadc0aa988d1d25
Greg Logan <gregorydlogan@gmail.com>
2021-11-19 14:33:46 -0600

    Removing this since it's no longer needed

--- docker-qa-images/ocqa-debian-repo-base/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-HOME=/mnt/aptly GNUPGHOME=/mnt/aptly/.gnupg gpg --import /mnt/aptly/signing.key
-
-HOME=/mnt/aptly GNUPGHOME=/mnt/aptly/.gnupg aptly -config=/mnt/aptly/aptly.conf api serve
